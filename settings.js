36738672522c82e1f1743f5b84a5a4f0e89acc10
Hallofsoul <99620476+Hallofsoul@users.noreply.github.com>
2022-04-13 12:03:07 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,9 +12,9 @@ global.APIKeys = {
 }
 
 // setting 
-global.autoread = false // auto read pesan / message
-global.autorecording = true //status auto merekam ( auto record )
-global.autoketik = false //status auto mengetik (auto typing)
+global.autoread = true // auto read pesan / message
+global.autorecording = false //status auto merekam ( auto record )
+global.autoketik = true //status auto mengetik (auto typing)
 global.available = false //status online (online)
 
 // Other
@@ -25,12 +25,12 @@ global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
 global.github = "https://zeeone-ofc.github.io/" //bebas
 global.email = "zeeoneofc@gmail.com" //bebas
 global.region = "Indonesia" //bebas
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.ownernomer = "6289529337047" // nomor wa kalian
+global.ownernomerr = "6289529337047" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62887435047326","62887435047326","6285342106390"] //ganti agar fitur owner bisa di gunakan
+global.owner = ["6282352239289","6289529337047","6285754058907"] //ganti agar fitur owner bisa di gunakan
 global.packname = '© Alphabot-Mdོ' //sticker wm ubah
 global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
 global.sessionName = 'session'
