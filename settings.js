97c20820f613fa2584f36d410bd9b277e6bd691a
Hallofsoul <99620476+Hallofsoul@users.noreply.github.com>
2022-04-16 19:35:42 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,21 +12,21 @@ global.APIKeys = {
 }
 
 // setting 
-global.autoread = true // auto read pesan / message
-global.autorecording = false //status auto merekam ( auto record )
-global.autoketik = true //status auto mengetik (auto typing)
-global.available = true //status online (online)
+global.autoread = false // auto read pesan / message
+global.autorecording = true //status auto merekam ( auto record )
+global.autoketik = false //status auto mengetik (auto typing)
+global.available = false //status online (online)
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
+global.botname = "Xbot-Mdོ" //namabot kalian
+global.ownername= "ᴹᴿ᭄ Wibuu ×፝֟͜×" //nama kalian
 global.myweb ="https://api-alphabot.herokuapp.com/" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
+global.youtube = "https://m.youtube.com/wibuu" //bebas asal jan hapus
 global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.email = "drackulx" //bebas
 global.region = "Indonesia" //bebas
-global.ownernomer = "6285754058907" // nomor wa kalian
-global.ownernomerr = "+6285754058907" //nmr wa kalian
+global.ownernomer = "6282352239289" // nomor wa kalian
+global.ownernomerr = "+6282352239289" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
